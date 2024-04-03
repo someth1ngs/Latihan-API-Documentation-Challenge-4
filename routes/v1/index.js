da66fc6aed39f91b8ec8071c211a56e7acc5ac7e
Dewangga <hamadakinose@gmail.com>
2024-04-03 22:00:26 +0700

    Simple Authorization Added

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -4,8 +4,26 @@ const accountController = require("../../controller/v1/accountController.js");
 const userController = require("../../controller/v1/userController.js");
 const transactionsController = require("../../controller/v1/transactionsController.js");
 
+function auth(req, res, next){
+    let {authorization} = req.headers;
+    
+    if (authorization){
+        let token = authorization.split(' ')[1];
+
+        if(token){
+            return next()
+        }
+    }
+
+    return res.status(401).json({
+        status: false,
+        message: "You're not authorized",
+        data: null
+    })
+}
+
 // API Users + Profile //
-router.post("/users", userController.store);
+router.post("/users", auth, userController.store);
 router.get("/users", userController.index);
 router.get("/users/:userId", userController.show);
 
